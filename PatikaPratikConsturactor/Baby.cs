70f17f37679511b2fe7b84a16f7f9bc81cc8a874
sefayilmaz <sefaylmaz14@gmail.com>
2024-08-01 18:07:44 +0300

    Add project files.

---
+++ PatikaPratikConsturactor/Baby.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaPratikConsturactor
+{
+    public class Baby
+    {
+        //Field tanımlamaları
+        private string _name;
+        private string _surname;
+        private DateTime _birthDate;
+        //Propetry tanımlamaları
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birth { get; set; }
+        //Default Constructor tanımlaması
+        public Baby()
+        {
+            Console.WriteLine("INGAAAAAAAAAAAA");
+            DateTime birthDate = DateTime.Now;// Ekrana ınga yazı geldikten sonra programın çalıştığı zamanı alan değişken
+            Console.WriteLine($"Doğum zamanı:{birthDate}");
+
+        }
+        //Parametre alan Constructor tanımlaması
+        public Baby(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            Console.WriteLine("INGAAAAAAAAAAAAAA");
+            DateTime birthDate = DateTime.Now;// Ekrana ınga yazı geldikten sonra programın çalıştığı zamanı alan değişken
+            Console.WriteLine($"Doğum zamanı: {birthDate}");
+
+        }
+        //Bilgileri konsola yazdıran method
+        public void Display()
+        {
+            Console.WriteLine($"Bebeğin İsmi:{Name} Soyismi:{Surname}");
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
---
+++ PatikaPratikConsturactor/Program.cs
@@ -0,0 +1,18 @@
+﻿using PatikaPratikConsturactor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Default Constructor ile bilgi girişi ve konsola yazdırma
+        Baby baby1 = new Baby();
+        baby1.Name = "Fahri";
+        baby1.Surname = "Korutürk";
+        baby1.Display();
+        //Parametre alan constructor ile bilgi girişi ve konsola yazdırma
+        Baby baby2 = new Baby("Sait Faik", "Abasıyanık");
+        baby2.Display();
+
+
+    }
+}
